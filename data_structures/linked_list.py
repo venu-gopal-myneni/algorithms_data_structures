8a87f849c44ab955b1ad5866cd95e8b9c06f23c7
myneni venu gopal <mailvenumyneni@gmail.com>
2023-11-14 22:19:27 +0530

    Add doubly linked list constructor from sequence

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -18,14 +18,35 @@ class Node:
 
     def __str__(self):
         if self.is_head and self.is_tail:
-            tag = "Single"
+            tag = "(Single)"
         elif self.is_head:
-            tag = "Head"
+            tag = "(Head)"
         elif self.is_tail:
-            tag = "Tail"
+            tag = "(Tail)"
         else:
             tag = ""
         return f"{tag} {self.data}"
+
+
+class DoublyLinkedList:
+    def __init__(self, seq=None):  # [1,2,3,4,5]
+        self.head = None
+        if seq is not None:
+            self.head = Node(seq[0])
+            cur_node = self.head
+            for ele in seq[1:]:
+                new_node = Node(ele)
+                new_node.previous = cur_node
+                cur_node.next = new_node
+                cur_node = new_node
+
+    def __str__(self):
+        out = str(self.head)
+        cur_node = self.head
+        while cur_node.next is not None:
+            out = f"{out} -> {cur_node.next}"
+            cur_node = cur_node.next
+        return out
 
 
 if __name__ == "__main__":
@@ -36,3 +57,6 @@ if __name__ == "__main__":
     print(node1)
     print(node2)
     print(node3)
+
+    ll = DoublyLinkedList([23, 12, 45, 67, 100])
+    print(ll)
